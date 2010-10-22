da60f086de5617df1faaeb0c3571915ba4613315
Harro van der Klauw <harro.vanderklauw@getlogic.nl>
2010-10-22 09:37:33 +0200

    ----------------------------------------------------------------------- r29 | chris.schaatsbergen | 2009-03-09 15:58:38 +0100 (ma, 09 mrt 2009) | 1 line
    Added revision to the presave

--- dev/system/record/plugins/CreatedUpdatedByPlugin.class.php
+++ dev/system/record/plugins/CreatedUpdatedByPlugin.class.php
@@ -8,11 +8,13 @@ class CreatedUpdatedByPlugin extends RecordPlugin {
 	public function preInsert(RecordEvent $event) {
 		// $this->record->setData('created', 'now'); //Filled by default?!
 		$this->record->setData('created_by', Developer::getInstance()->getId());
+		// $this->record->setData('revision', '1'); //Filled by default?!
 	}
 	
 	public function preSave(RecordEvent $event) {
 		$this->record->setData('updated', 'now');
 		$this->record->setData('updated_by', Developer::getInstance()->getId());
+		$this->record->setData('revision', $this->record->getData('revision')++);
 	}
 }
 
