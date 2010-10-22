86fe13313f99f7dbbe7168479642a5050197302c
Harro van der Klauw <harro.vanderklauw@getlogic.nl>
2010-10-22 09:37:36 +0200

    ----------------------------------------------------------------------- r32 | chris.schaatsbergen | 2009-03-10 12:43:51 +0100 (di, 10 mrt 2009) | 1 line
    Add the extra keys needed to the records config

--- dev/system/record/plugins/CreatedUpdatedByPlugin.class.php
+++ dev/system/record/plugins/CreatedUpdatedByPlugin.class.php
@@ -5,16 +5,33 @@ require_once 'Developer.class.php';
 
 class CreatedUpdatedByPlugin extends RecordPlugin {
 
+	public function __construct($record, array $options) {
+		parent::__construct($record, $options);
+		// Add the extra columns required to the records config
+		if (!array_key_exists('created', $record->config))
+			$record->config['created'] = array('writability' => self::READONLY);
+		if (!array_key_exists('created_by', $record->config))
+			$record->config['created_by'] = array('writability' => self::READONLY);
+		if (!array_key_exists('updated', $record->config))
+			$record->config['updated'] = array('writability' => self::READONLY);
+		if (!array_key_exists('updated_by', $record->config))
+			$record->config['updated_by'] = array('writability' => self::READONLY);		
+		if (!array_key_exists('revision', $record->config))
+			$record->config['revision'] = array('writability' => self::READONLY);
+	}
+	
 	public function preInsert(RecordEvent $event) {
 		// $this->record->setData('created', 'now'); //Filled by default?!
 		$this->record->setData('created_by', Developer::getInstance()->getId());
 		// $this->record->setData('revision', '1'); //Filled by default?!
+		parent::preInsert($event);
 	}
-	
+
 	public function preSave(RecordEvent $event) {
 		$this->record->setData('updated', 'now');
 		$this->record->setData('updated_by', Developer::getInstance()->getId());
 		$this->record->setData('revision', $this->record->getData('revision')++);
+		parent::preSave($event);
 	}
 }
 
